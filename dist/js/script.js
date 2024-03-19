786d4fefe364e80a4d00e3062bede1976a70c90e
RonK-0 <ronkua12@gmail.com>
2024-03-20 06:55:15 +0800

    setup

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,40 +7,26 @@ burgir.addEventListener("click", ()=>{
     bodih.classList.toggle("open");
 });
 
-const links = document.querySelectorAll(".links__item h4");
-links.forEach((link) => {
-  link.addEventListener("click", () => {
-    link.querySelector("i").classList.toggle("open");
-    link.nextElementSibling.classList.toggle("open");
-  });
-});
+// const links = document.querySelectorAll(".links__item h4");
+// links.forEach((link) => {
+//   link.addEventListener("click", () => {
+//     link.querySelector("i").classList.toggle("open");
+//     link.nextElementSibling.classList.toggle("open");
+//   });
+// });
 
-const starNav = document.querySelectorAll(".coffee__nav__link");
-const starContent = document.querySelectorAll(".coffee__item");
-starNav.forEach((star) => {
-  star.addEventListener("click", () => {
-    // removeActiveStar();
-    starNav.forEach((star) => {
-        star.classList.remove("active");
-      });
-    star.classList.add("active");
-    const activeContent = document.querySelector(`#${star.id}-content`);
-    // removeActiveContent();
-    starContent.forEach((content) => {
-        content.classList.remove("active");
-      });
-    activeContent.classList.add("active");
-  });
-});
-
-function removeActiveStar() {
-  starNav.forEach((star) => {
-    star.classList.remove("active");
-  });
-}
-
-function removeActiveContent() {
-    starContent.forEach((content) => {
-      content.classList.remove("active");
-    });
-}
+// const starNav = document.querySelectorAll(".coffee__nav__link");
+// const starContent = document.querySelectorAll(".coffee__item");
+// starNav.forEach((star) => {
+//   star.addEventListener("click", () => {
+//     starNav.forEach((star) => {
+//         star.classList.remove("active");
+//       });
+//     star.classList.add("active");
+//     const activeContent = document.querySelector(`#${star.id}-content`);
+//     starContent.forEach((content) => {
+//         content.classList.remove("active");
+//       });
+//     activeContent.classList.add("active");
+//   });
+// });
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,26 +4,20 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage:{
-        starBanner: "url('../dist/img/rewards/xl-hero-desktop_2021.png')",
-        starBannerMobile: "url('../dist/img/rewards/hero-mobile_2021.jpg')"
+        bannah: "url('../dist/img/banner-img.png')",
+        logo: "url('../dist/img/logo.png')"
       },
       colors:{
         dark: "#2b2b2b",
         light: "#fff",
-        accent: "#006341",
-        accent_alt: "#1e3932",
-        c1: "#d5d0ea",
-        c2: "#f2f0eb",
-        c3: "#cee8c9",
-        c4: "#f5cac4",
-        c5: "#dbe6f2",
-        c6: "#d5d0ea",
-        c7: "#e99264",
-        c8: "#006241",
-        el_verde:"#d4e9e2"
+        grayy: "#959595",
+        accent: "#fb4d3c",
+        mail: "#e69a11",
+        twitter: "#26a6de",
+        facebook: "#1779f4",
       },
       fontFamily:{
-        'boldB':["SoDoSans Bold"]
+        
       }
     },
   },
